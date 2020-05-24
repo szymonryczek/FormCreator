f820c5c5e42e421ff2b2922b4c9c44be8609ba95
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-05-24 20:16:33 +0200

    Previewer style select

--- src/modules/Previewer/Previewer.tsx
+++ src/modules/Previewer/Previewer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { getFormFields } from '~/store';
 import {
+  createStyles,
+  FormControl,
   Grid,
   InputLabel,
   MenuItem,
@@ -9,36 +11,81 @@ import {
   Select,
   Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 150,
+    },
+  }),
+);
 
 export const Previewer = () => {
+  const classes = useStyles();
   const formFields = useSelector(getFormFields);
 
+  const [fieldType, setFieldType] = React.useState<string | number>('');
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const type = event.target.value as string;
+
+    setFieldType(type);
+  };
+
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={1}>
       {formFields &&
         formFields.map((formField) => {
           return (
             <Grid item xs={12}>
               {!formField.type && (
-                <Paper variant="outlined">
-                  <Typography>Select type</Typography>
+                <Paper
+                  variant="elevation"
+                  style={{
+                    height: '56px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Typography>Select field type to display</Typography>
                 </Paper>
               )}
 
               {formField.type === 'Select' && (
-                <>
-                  <InputLabel id="demo-controlled-open-select-label">
-                    {formField.label}
-                  </InputLabel>
-                  <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                  </Select>
-                </>
+                <div style={{ height: '56px' }}>
+                  <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-controlled-open-select-label">
+                      {formField.label}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-controlled-open-select-label"
+                      id="demo-controlled-open-select"
+                      open={open}
+                      onClose={handleClose}
+                      onOpen={handleOpen}
+                      value={fieldType}
+                      onChange={handleChange}
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={'m'}>Mezczyzna</MenuItem>
+                      <MenuItem value={'k'}>Kobieta</MenuItem>
+                      <MenuItem value={'i'}>Inna</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
               )}
             </Grid>
           );
