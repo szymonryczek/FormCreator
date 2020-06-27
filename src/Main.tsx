4ae23fc5269ce1fc8a6b0daa8b5626cb1dc9c927
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-06-28 00:23:26 +0200

    Get form from id query

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Card,
   createStyles,
@@ -7,9 +7,11 @@ import {
   CardContent,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { AppHeader, Fabs } from '~/components';
 import { Builder, Previewer } from '~/modules';
+import { getForm } from './utils';
+import { loadForm } from './store';
+import { useDispatch } from 'react-redux';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,6 +27,18 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const Main = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const documentQueryID = urlParams.get('id');
+
+  useEffect(() => {
+    if (documentQueryID) {
+      const newForm = getForm(documentQueryID);
+      dispatch(loadForm(newForm));
+    }
+  }, [dispatch, documentQueryID]);
 
   return (
     <>
